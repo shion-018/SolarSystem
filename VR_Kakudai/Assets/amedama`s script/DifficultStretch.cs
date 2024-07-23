bc43dd6c5ae2ba23c466a7861b204ec353cc15b1
AmedamaSpica <spica96amedama@gmail.com>
2024-07-23 14:49:32 +0900

    今日の、commit

--- VR_Kakudai/Assets/amedama`s script/DifficultStretch.cs
+++ VR_Kakudai/Assets/amedama`s script/DifficultStretch.cs
@@ -5,18 +5,29 @@ using UnityEngine;
 public class DifficultStretch : MonoBehaviour
 {
 
-    [SeliarizeFierd]
+    private Vector3 BeforeScale;
+    private Vector3 AfterScale;
+    [SerializeField] private float stretch = 1.0f;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        BeforeScale = transform.localScale;
+        AfterScale = transform.localScale;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        AfterScale = transform.localScale;
+
+        if (AfterScale != BeforeScale)
+        {
+            transform.localScale *= stretch;
+            
+        }
+
+        BeforeScale = transform.localScale;
     }
 }
