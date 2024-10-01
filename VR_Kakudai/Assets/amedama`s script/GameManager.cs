2cc5fef9d9d77f91f545e4a4369028551194dcb8
AmedamaSpica <spica96amedama@gmail.com>
2024-10-01 14:06:26 +0900

    ぽぽぽぽぽぽぽぽぽぽぽぽぽぽぽっぽぽぽ

--- VR_Kakudai/Assets/amedama`s script/GameManager.cs
+++ VR_Kakudai/Assets/amedama`s script/GameManager.cs
@@ -4,40 +4,70 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.Timeline;
 
+
+
 public class GameManager : MonoBehaviour
 {
 
     [SerializeField, Tooltip("タイムリミット（秒）")] int TimeLimit = 60;
     [SerializeField, Tooltip("制限時間Text")] TextMeshProUGUI TimeLimitText;
+    [SerializeField, Tooltip("")]
+
+    public enum GAMESTATE
+    {
+        Title,
+        Movie,
+        Play,
+        Pose,
+        GameClear,
+        GameEnd,
+    }
+
+    public GAMESTATE gamestate = GAMESTATE.Title;
+
     int TimeNow = 0;
     float Seconds_If;
 
     // Start is called before the first frame update
     void Start()
     {
+
         
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Seconds_If += Time.deltaTime;
-        if(Seconds_If >= 1.0f )
-        {
-            TimeNow += 1;
-            TimeLimitText.text = TimeNow.ToString();
-            Seconds_If = 0;
+        
 
-            if(TimeLimit <= TimeNow)
+        if (Input.GetKey(KeyCode.Space))
+        {
+            gamestate = GAMESTATE.Play;
+        }
+
+        if (gamestate == GAMESTATE.Play)
+        {
+            Seconds_If += Time.deltaTime;
+
+            if (Seconds_If >= 1.0f)
             {
-                TimeNow = 0;
+                TimeNow += 1;
+                TimeLimitText.text = TimeNow.ToString();
+                Seconds_If = 0;
+
+                if (TimeLimit <= TimeNow)
+                {
+                    TimeNow = 0;
+
+                }
 
             }
-
-
         }
-        
+
     }
 
     
+
+
 }
