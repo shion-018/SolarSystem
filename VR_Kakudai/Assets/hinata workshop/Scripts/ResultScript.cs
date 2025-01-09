bc16227df399c02560109205be4ed4b9d220192f
shion-018 <noname09514@yahoo.co.jp>
2025-01-09 17:26:27 +0900

    リザルトの大枠は完成

--- VR_Kakudai/Assets/hinata workshop/Scripts/ResultScript.cs
+++ VR_Kakudai/Assets/hinata workshop/Scripts/ResultScript.cs
@@ -24,13 +24,13 @@ public class ResultScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        resultCanvas.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     void EditorResult()
     {
@@ -46,12 +46,12 @@ public class ResultScript : MonoBehaviour
     }
     public void IsGameClear()
     {
-        StartCoroutine(RetryAfterDelay(10f)); //十秒後にシーンをリロード
+        StartCoroutine(RetryAfterDelay(10f)); //10秒後にシーンをリロード
     }
     private IEnumerator RetryAfterDelay(float delay)
     {
-        yield return new WaitForSeconds(delay); 
-        RetryGame(); 
+        yield return new WaitForSeconds(delay);
+        RetryGame();
     }
     void RetryGame()
     {
@@ -60,20 +60,21 @@ public class ResultScript : MonoBehaviour
     void Result()
     {
         //点数の判別欄
-        sumResult = goodResult*3 + badResult;
+        sumResult = goodResult * 3 + badResult;
+
         if (sumResult >= 15)
         {
             god.SetActive(true);
         }
-        if (sumResult >= 10 &&  sumResult < 15)
+        if (sumResult >= 10 && sumResult < 15)
         {
             excerent.SetActive(true);
         }
-        if(sumResult >= 5 && sumResult < 10)
+        if (sumResult >= 5 && sumResult < 10)
         {
             good.SetActive(true);
         }
-        if( sumResult >= 0 && sumResult < 5)
+        if (sumResult < 5)
         {
             close.SetActive(true);
         }
