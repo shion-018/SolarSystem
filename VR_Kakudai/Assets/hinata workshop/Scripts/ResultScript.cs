39d6be5262ac0f70d2a2e141577eab848d558ac0
shion-018 <noname09514@yahoo.co.jp>
2025-01-09 16:12:07 +0900

    リザルト準備

--- VR_Kakudai/Assets/hinata workshop/Scripts/ResultScript.cs
+++ VR_Kakudai/Assets/hinata workshop/Scripts/ResultScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ResultScript : MonoBehaviour
@@ -35,5 +36,18 @@ public class ResultScript : MonoBehaviour
         goodResul_text.text = "" + goodResult;
         badResul_text.text = "" + badResult;
         sumResult_text.text = "" + sumResult;
+    }
+    public void IsGameClear()
+    {
+        StartCoroutine(RetryAfterDelay(10f)); 
+    }
+    private IEnumerator RetryAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay); 
+        RetryGame(); 
+    }
+    void RetryGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
